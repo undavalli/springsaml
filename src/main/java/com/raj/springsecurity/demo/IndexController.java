0ea01795f0c783466216eb6bc4d300f80fa3f4e3
prudhviraj <prudhviraj29@gmail.com>
2018-05-05 23:02:10 -0500

    first commit

---
+++ src/main/java/com/raj/springsecurity/demo/IndexController.java
@@ -0,0 +1,14 @@
+package com.raj.springsecurity.demo;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class IndexController {
+	
+	@RequestMapping("/")
+	public String index(){
+		return "Hello Saml";
+	}
+
+}
---
+++ src/main/java/com/raj/springsecurity/demo/SpringSaml.java
@@ -0,0 +1,14 @@
+package com.raj.springsecurity.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class SpringSaml {
+	
+	public static void main(String[] args){
+		SpringApplication.run(SpringSaml.class, args);
+	}
+
+}
